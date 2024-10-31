9ed64ecce6013edbc9bdc8745e271d59ba6bfd6f
Hyunmin Park <753429@student.dusd.net>
2024-10-31 15:23:24 -0700

    mesa need clock mesa confused
    why is it so hard

---
+++ VrRoomParkP7/Assets/Scripts/ClockScript.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Utilities.Tweenables.Primitives;
+
+public class ClockScript : MonoBehaviour
+{
+    public GameObject hourHand;
+    public GameObject minuteHand;
+    public GameObject secondHand;
+    DateTime dateTime = DateTime.Now;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
