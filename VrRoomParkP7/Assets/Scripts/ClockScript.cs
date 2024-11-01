c4a18edbfcd23e472e3a6a5cbbf1da40ed12c239
Hyunmin Park <753429@student.dusd.net>
2024-10-31 19:01:28 -0700

    mesa not know the hours work but nothing else
    stupid clock

--- VrRoomParkP7/Assets/Scripts/ClockScript.cs
+++ VrRoomParkP7/Assets/Scripts/ClockScript.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,14 +11,23 @@ public class ClockScript : MonoBehaviour
     public GameObject minuteHand;
     public GameObject secondHand;
     DateTime dateTime = DateTime.Now;
+    public float rotateSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        int hoursInClock = 360 / 12;
+        int minutes = 360 / 1440;
+
+        int currentSeconds = DateTime.Now.Second;
+        int currentMinutes = DateTime.Now.Minute;
+        int currentHour = DateTime.Now.Hour;
+        hourHand.transform.Rotate(0, currentHour * hoursInClock , 0);
+        minuteHand.transform.Rotate(0, currentMinutes * minutes, 0);
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
     }
